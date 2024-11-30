7880d39f19dc46767b1a6d11e88d04ca2d4fd89d
patient4 <rachit.mohan2112@gmail.com>
2024-11-30 19:52:01 +0530

    added class based tests

---
+++ source/shape.py
@@ -0,0 +1,14 @@
+"""
+This file contains a Shape Class it will be having 2 attributes
+Area()
+Perimeter()
+inheriting class will implement these methods and get the area and perimeter as per requirements.
+"""
+class Shape:
+    def __init__(self):
+        pass
+    def area(self):
+        pass
+    def perimeter(self):
+        pass
+
